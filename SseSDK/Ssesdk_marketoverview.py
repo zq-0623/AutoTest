644f4b4a4c6bd09a56d2dd14c910b4e1899f070e
zq-0623 <1209405215@qq.com>
2023-08-09 22:23:40 +0800

    修改分K请求

--- SseSDK/Ssesdk_marketoverview.py
+++ SseSDK/Ssesdk_marketoverview.py
@@ -6,18 +6,21 @@ from Ssesdk_tool import SseOptionQuote
 
 def request_run():
     headers = {
-        "token": "MitakeWeb"
+        "token": "MitakeWeb",
+        "symbol":"600000.sh",
+        "param":"20230809090000"
     }
     response = SseOptionQuote(url, headers=headers)
     res = response.text.replace("\x03", "").split("\x02")
-    for i in res:
-        if i == '':
-            res.remove('')
+    # for i in res:
+    #     if i == '':
+    #         res.remove('')
+    print(f"response_list1=====>'{response.text}'")
     print(f"response_list1=====>'{res}'")
 
 
 if __name__ == '__main__':
-    url = "http://114.80.155.61:22016/v1/marketoverview"
+    url = "http://114.80.155.61:22016/v3/m1"
     request_run()
 
 
