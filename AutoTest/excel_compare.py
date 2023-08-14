1a6ff24fd864bfb9875c25823a8b4a508344b3fb
zq-0623 <1209405215@qq.com>
2023-08-14 18:23:30 +0800

    add logging

--- AutoTest/excel_compare.py
+++ AutoTest/excel_compare.py
@@ -1,5 +1,7 @@
-import pandas as pd
+# -*- coding: utf-8 -*-
 
+import pandas as pd
+from util.logTool import logger
 
 def compare_excel_columns(file1_path, file2_path, file1_column, file2_column):
     # 读取文件内容
@@ -29,18 +31,49 @@ def compare_excel_columns(file1_path, file2_path, file1_column, file2_column):
 
     return different_results
 
+# mqtt
+# r均价(a)	r最新价(c)	r第一笔资料时间(ot)	r最高价(h)	r索引(i)	r最低价(l)
+# r成交额(m)	r开盘价(o)	r资料确认时间(dt)	r参考价(r)	r时间(t)	r成交量(v)	r参考价(ir)	r最新价(ic)
 
+# http
+# 日期时间	开	高	低	收	量	参考价	成交额	盘后成交量	盘后成交额	前一根收盘价	基金净值	均价
+
+'''
+000656
+131810
+159792
+200625
+204001
+300059
+513060
+600050
+688223
+837592
+900901
+10005650
+90002344
+H30455
+'''
+
+
+mqtt_file_path = r'C:\Users\Administrator\Desktop\mqtt_kline_data\min1_mqtt_line_H30455.xlsx'
+http_file_path = r'C:\Users\Administrator\Desktop\http_kline_data\min1_http_line_H30455.xlsx'
 # 文件和列名列表
 file_pairs = [
-    (r'D:\workspace\PythonProject\AutoTest\AutoTest\Data\mqtt_line.xlsx', 'D:\workspace\PythonProject\AutoTest\AutoTest\Data\http_line.xlsx', 'r最高价(h)', '高'),
-    (r'D:\workspace\PythonProject\AutoTest\AutoTest\Data\mqtt_line.xlsx', 'D:\workspace\PythonProject\AutoTest\AutoTest\Data\http_line.xlsx', 'r最低价(l)', '低'),
-    (r'D:\workspace\PythonProject\AutoTest\AutoTest\Data\mqtt_line.xlsx', 'D:\workspace\PythonProject\AutoTest\AutoTest\Data\http_line.xlsx', 'r时间(t)', '日期时间'),
+    (mqtt_file_path, http_file_path, 'r均价(a)', '均价'),
+    (mqtt_file_path, http_file_path, 'r最高价(h)', '高'),
+    (mqtt_file_path, http_file_path, 'r最低价(l)', '低'),
+    (mqtt_file_path, http_file_path, 'r成交额(m)', '成交额'),
+    (mqtt_file_path, http_file_path, 'r开盘价(o)', '开'),
+    (mqtt_file_path, http_file_path, 'r参考价(r)', '参考价'),
+    (mqtt_file_path, http_file_path, 'r时间(t)', '日期时间'),
+    (mqtt_file_path, http_file_path, 'r成交量(v)', '量'),
+    (mqtt_file_path, http_file_path, 'r最新价(ic)', '基金净值'),
     # 添加更多文件和列名组合
 ]
 
 # 循环比较并打印结果
 for file1_path, file2_path, file1_column, file2_column in file_pairs:
     result = compare_excel_columns(file1_path, file2_path, file1_column, file2_column)
-    print(f"比较 {file1_path} 中的 '{file1_column}' 和 {file2_path} 中的 '{file2_column}':")
-    print(result)
-    print("-" * 30)
+    logger.info(f"比较 {file1_path} 中的 '{file1_column}' 和 {file2_path} 中的 '{file2_column}':")
+    logger.info(result)
---
+++ util/logTool.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+import logging
+import time,os
+
+import colorlog as colorlog
+
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+print(time.strftime("%y%m%d%H%M%S"))
+
+# 创建日志文件路径
+LOG_PATH = os.path.join(BASE_PATH,"log")
+if not os.path.exists(LOG_PATH):
+    os.mkdir(LOG_PATH)
+
+
+class Logger():
+    def __init__(self):
+        # 创建以年月日为日志文件名
+        self.logname = os.path.join(LOG_PATH, "{}.log".format(time.strftime("%y%m%d")))
+
+        log_colors_config = {
+            'DEBUG': 'white',  # cyan white
+            'INFO': 'green',
+            'WARNING': 'yellow',
+            'ERROR': 'red',
+            'CRITICAL': 'bold_red',
+        }
+
+        # 创建日志对象
+        self.logger = logging.getLogger()
+        # 设置日志显示级别
+        self.logger.setLevel(logging.DEBUG)
+
+        # 添加控制台，用于输出日志到控制台
+        self.console_handler = logging.StreamHandler()
+        # 设置控制台输出日志级别
+        self.console_handler.setLevel(logging.DEBUG)
+
+        # 添加日志文件，用于输出日志文件
+        self.file_handler = logging.FileHandler(self.logname, mode="a", encoding="utf-8")
+        # 设置文件输出日志级别
+        self.file_handler.setLevel(logging.DEBUG)
+
+        # 将handler添加至日志器中
+        self.logger.addHandler(self.console_handler)
+        self.logger.addHandler(self.file_handler)
+
+        # 自定义日志显示格式
+        self.file_formatter = logging.Formatter(
+            '%(asctime)s--%(filename)s %(lineno)d--%(levelname)s]: %(message)s'
+        )
+        self.console_formatter = colorlog.ColoredFormatter(
+            fmt= '%(asctime)s--%(filename)s %(lineno)d--%(levelname)s]: %(message)s',
+            log_colors=log_colors_config
+        )
+
+        # 将自定义日志格式赋予日志器handler
+        self.console_handler.setFormatter(self.console_formatter)
+        self.file_handler.setFormatter(self.file_formatter)
+
+
+logger = Logger().logger
+
+
+logger.debug("==========调试信息==========")
+logger.info("==========调试信息==========")
+logger.warning("==========调试信息==========")
+logger.error("==========调试信息==========")
+logger.critical("==========调试信息==========")
