1f640b084defd2f4c72eea88b403ddbed1aaf473
zq-0623 <1209405215@qq.com>
2023-09-20 18:43:10 +0800

    updata files

--- AutoTest/get_data_html.py
+++ AutoTest/get_data_html.py
@@ -1,12 +1,17 @@
 from bs4 import BeautifulSoup
 import urllib.request,urllib.error
 
-def getData():
+def getData(baseurl):
     datalist = []
-    pass
+    askUrl(baseurl)
+    soup = BeautifulSoup(baseurl,'lxml')
+    div_list = soup.find_all("div",class_= "panel primary-panel")
+    print(div_list)
 
 
-def askUrl(url):
+
+
+def askUrl(requesturl):
     header = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"
     }
@@ -14,15 +19,15 @@ def askUrl(url):
     html = ""
     try:
         response = urllib.request.urlopen(request)
-        html = response.read().decode("GB2312")
+        html = response.read().decode("utf-8")
     except urllib.error.URLError as e:
         if hasattr(e,"code"):
             print("status ===>",e.code)
         if hasattr(e,"reason"):
             print("reason ===>",e.reason)
-    print(html)
+    return html
 
 
 if __name__ == '__main__':
-    url = "http://www.cwl.gov.cn/ygkj/wqkjgg/ssq/"
-    askUrl(url)
+    url = "https://caipiao.eastmoney.com/pub/Result/History/ssq?page=1"
+    getData(url)
