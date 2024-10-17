4b583af6c5c75beb1917ffeb37b43879c5db3f24
Administrator <1209405215@qq.com>
2024-10-17 14:55:53 +0800

    优化分时明细对比接口

--- SseSDK/test.py
+++ SseSDK/test.py
@@ -24,58 +24,138 @@
 #                 # print(new_list[2])  # 总股本
 #                 print(new_list[3])  # 流通股本
 
+#
+# import redis
+# import requests
+#
+# # 设置接口地址和 Redis 连接
+# url = "http://114.28.169.157:22017/v1/ahquote"
+# redis_host = 'localhost'
+# redis_port = 6379
+# r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+#
+# def fetch_and_process_data():
+#     try:
+#         # 请求接口数据
+#         response = requests.get(url)
+#         response.raise_for_status()  # 检查请求是否成功
+#         data = response.content.decode('utf-8')  # 将返回数据解码为字符串
+#
+#         # 以"\x03"分割数据
+#         split_data = data.split("\x03")
+#         field_values = []
+#
+#         # 遍历分割后的数据，再以"\x02"分割并提取第二个字段
+#         for item in split_data:
+#             parts = item.split("\x02")
+#             if len(parts) > 1:
+#                 field_values.append(parts[1])
+#
+#         # 遍历字段列表，将每个值按"."分割，并用分割后的第二个部分进行Redis查询
+#         for value in field_values:
+#             dot_split = value.split(".")
+#             if len(dot_split) > 1:
+#                 first_part = dot_split[0]
+#                 second_part = dot_split[1]
+#                 # 从 Redis 获取与 second_part 相关的值
+#                 redis_value = r.get(second_part)
+#                 if redis_value:
+#                     # 拼接成新的 redis_key.redis_value.first_part 形式
+#                     redis_key = f"{second_part}.{redis_value}.{first_part}"
+#                     A_rate = r.get("hk.cr.CNY")
+#                     H_rate = r.get("hk.cr.HKD")
+#                     print(A_rate,H_rate)
+#                     print(f"Constructed Redis Key: {redis_key}")
+#                 else:
+#                     print(f"Redis key '{second_part}' not found in Redis.")
+#             else:
+#                 print(f"Invalid value (no '.'): {value}")
+#
+#     except requests.RequestException as e:
+#         print(f"Error fetching data: {e}")
+#     except redis.RedisError as e:
+#         print(f"Redis error: {e}")
+#
+# # 调用函数
+# fetch_and_process_data()
 
-import redis
-import requests
+#
+# import matplotlib.pyplot as plt
+# import pandas as pd
+#
+# # 设置字体，确保中文显示
+# plt.rcParams['font.sans-serif'] = ['SimHei']
+# plt.rcParams['axes.unicode_minus'] = False
+#
+# # 加载 CSV 文件到 DataFrame 中 (修改为你的文件路径)
+# df = pd.read_csv('./TestFile/process_monitor_2_50.csv')
+#
+# # 去除列名中的空格
+# df.columns = df.columns.str.strip()
+#
+# # 确保 'Timestamp' 列被正确解析为时间格式
+# df['Timestamp'] = pd.to_datetime(df['Timestamp'], format='%H:%M:%S')
+#
+# # 清理 RES 列，去除空格和单位，并转换为浮点数
+# df['RES'] = df['RES'].str.strip().str.replace('g', '').astype(float)
+#
+# # 绘制 %CPU 趋势图
+# plt.figure(figsize=(20, 6))
+# plt.plot(df['Timestamp'], df['%CPU'], label='CPU使用率', marker='o')
+# plt.xlabel('时间')
+# plt.ylabel('数值')
+# plt.title('50只股票3秒间隔的CPU使用率趋势')
+# plt.xticks(ticks=df['Timestamp'][::10], labels=df['Timestamp'].dt.strftime('%H:%M:%S')[::10], rotation=45)
+# plt.yticks(ticks=range(0, int(df['%CPU'].max()) + 50, 50))
+# plt.legend()
+# plt.tight_layout()
+# plt.show()
 
-# 设置接口地址和 Redis 连接
-url = "http://114.28.169.157:22017/v1/ahquote"
-redis_host = 'localhost'
-redis_port = 6379
-r = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
 
-def fetch_and_process_data():
-    try:
-        # 请求接口数据
-        response = requests.get(url)
-        response.raise_for_status()  # 检查请求是否成功
-        data = response.content.decode('utf-8')  # 将返回数据解码为字符串
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import pandas as pd
 
-        # 以"\x03"分割数据
-        split_data = data.split("\x03")
-        field_values = []
+# 读取 Excel 文件
+file_path = './TestFile/compare.xlsx'  # 替换为实际文件路径
+df = pd.read_excel(file_path)
 
-        # 遍历分割后的数据，再以"\x02"分割并提取第二个字段
-        for item in split_data:
-            parts = item.split("\x02")
-            if len(parts) > 1:
-                field_values.append(parts[1])
+# 将 '时间' 列转换为 datetime 对象
+df['时间'] = pd.to_datetime(df['时间'], format='%Y%m%d-%H:%M:%S')
 
-        # 遍历字段列表，将每个值按"."分割，并用分割后的第二个部分进行Redis查询
-        for value in field_values:
-            dot_split = value.split(".")
-            if len(dot_split) > 1:
-                first_part = dot_split[0]
-                second_part = dot_split[1]
-                # 从 Redis 获取与 second_part 相关的值
-                redis_value = r.get(second_part)
-                if redis_value:
-                    # 拼接成新的 redis_key.redis_value.first_part 形式
-                    redis_key = f"{second_part}.{redis_value}.{first_part}"
-                    A_rate = r.get("hk.cr.CNY")
-                    H_rate = r.get("hk.cr.HKD")
-                    print(A_rate,H_rate)
-                    print(f"Constructed Redis Key: {redis_key}")
-                else:
-                    print(f"Redis key '{second_part}' not found in Redis.")
-            else:
-                print(f"Invalid value (no '.'): {value}")
+# 设置时间列为索引
+df.set_index('时间', inplace=True)
 
-    except requests.RequestException as e:
-        print(f"Error fetching data: {e}")
-    except redis.RedisError as e:
-        print(f"Redis error: {e}")
+# 按10秒间隔重采样数据，使用前向填充法补充数据
+df_resampled = df.resample('60S').ffill()
 
-# 调用函数
-fetch_and_process_data()
+# 设置字体为支持中文的字体，如黑体 SimHei
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用于显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用于正常显示负号
 
+# 创建图形
+plt.figure(figsize=(30, 10))
+
+# 绘制折线图
+plt.plot(df_resampled.index, df_resampled['生产'], label='生产', marker='o')
+plt.plot(df_resampled.index, df_resampled['T6'], label='T6', marker='o')
+
+# 设置横轴显示时间格式
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+
+# 动态设置 x 轴的显示范围，选择每隔5个数据点显示一个时间刻度
+plt.xticks(df_resampled.index[::3], rotation=45)
+
+# 添加标题和标签
+plt.title('生产和T6随时间变化图')
+plt.xlabel('时间')
+plt.ylabel('值')
+
+# 显示图例
+plt.legend()
+
+# 显示网格
+plt.grid(True)
+
+# 显示图形
+plt.show()
--- quoteCompare/tick.py
+++ quoteCompare/tick.py
@@ -20,11 +20,13 @@ write_path = curPath + f'\\result\\tickResult\\{time1}_明细比对结果.xlsx'
 
 stock_file_mapping = {
     'sh': r'../testCase/AllStock/AllStock_sh.txt',
-    'sz': r'../testCase/AllStock/AllStock_sz.txt',
-    'bz': r'../testCase/AllStock/AllStock_bz.txt',
-    'csi': r'../testCase/AllStock/AllStock_csi.txt',
-    'hk': r'../testCase/AllStock/AllStock_hk.txt',
+    # 'sz': r'../testCase/AllStock/AllStock_sz.txt',
+    # 'bz': r'../testCase/AllStock/AllStock_bz.txt',
+    # 'csi': r'../testCase/AllStock/AllStock_csi.txt',
+    # 'hk': r'../testCase/AllStock/AllStock_hk.txt',
 }
+list1_data = []
+list2_data = []
 
 
 def quote_yaml(path):
@@ -38,9 +40,10 @@ tick_url = quote_yaml(tick_url)
 
 def compare_lists(list1, list2, symbol, url1, url2, results):
     fields = [list(item.keys())[0] for item in tick]
-    diff = DeepDiff(list1, list2)
+    diff = DeepDiff(list1, list2, ignore_order=True)
     if not diff:
         print("{}对比结果一致".format(symbol))
+        results.append("{}对比完全一致".format(symbol))
     else:
         if 'values_changed' in diff:
             values_changed = diff['values_changed']
@@ -49,79 +52,118 @@ def compare_lists(list1, list2, symbol, url1, url2, results):
                 field_name = fields[index]
                 old_value = change['old_value']
                 new_value = change['new_value']
-                results.append({'代码': symbol, '字段': field_name, url1: old_value, url2: new_value})
-                print('代码：{}，字段：{}，环境1：{}，环境2：{}'.format(symbol, field_name, old_value, new_value))
+                # results.append({'代码': symbol, '字段': field_name, url1: old_value, url2: new_value})
+                print('代码：{}，不一致的字段：{}，环境1：{}，环境2：{}'.format(symbol, field_name, old_value, new_value))
+
+        if 'iterable_item_added' in diff:
+            items_added = diff['iterable_item_added']
+            for key, new_value in items_added.items():
+                # print("iterable_item_added_new_value:",new_value)
+                # results.append({'代码': symbol, url1: None, url2: new_value})
+                print('代码：{}，环境2比环境1多出的字段：{}'.format(symbol, new_value, ))
+
+        if 'iterable_item_removed' in diff:
+            items_removed = diff['iterable_item_removed']
+            for key, old_value in items_removed.items():
+                # print("iterable_item_removed_old_value:",old_value)
+                # results.append({'代码': symbol, url1: old_value, url2: None})
+                print('代码：{}，环境2比环境1少的字段：{}'.format(symbol, old_value))
 
 
-async def fetch_data(session, url, headers):
-    try:
-        async with session.get(url, headers=headers) as response:
-            if response.status == 200:
-                response_text = await response.text()
-                response_str = response_text.split('\03')
-                if response_str[-1] == '':
-                    response_str.remove('')
-                all_data = []
-                if response_str:
-                    for m in range(len(response_str)):
-                        split_list = response_str[m].split("\x02")
-                        for i in range(len(split_list)):
-                            data_key = list(tick[i].keys())[0]
-                            data_value = list(tick[i].values())[0]
-                            if data_value == 'Y':
-                                split_list[i] = decode(split_list[i])
+async def fetch_data(url, headers):
+    async with aiohttp.ClientSession() as session:
+        while True:
+            try:
+                async with session.get(url, headers=headers) as response:
+                    if response.status == 200:
+                        response_text = await response.text()
+                        response_str = response_text.split('\03')
+                        if response_str[-1] == '':
+                            response_str.remove('')
+                        all_data = []
+                        if response_str:
+                            for m in range(len(response_str)):
+                                split_list = response_str[m].split("\x02")
+                                tick_dict = {}
+                                for i in range(len(split_list)):
+                                    data_key = list(tick[i].keys())[0]
+                                    data_value = list(tick[i].values())[0]
+                                    if data_value == 'Y':
+                                        split_list[i] = decode(split_list[i])
+                                    else:
+                                        split_list[i] = split_list[i]
+                                        # tick_dict[data_key] = split_list[i]
+                                all_data.append(split_list)
                         params_header = response.headers.get('params', None)
-                        second_value = params_header.split(',')[1]
-                        headers['param'] = '{},100,1'.format(second_value)
-                        all_data.append(split_list)
-                return all_data
-    except Exception as e:
-        print(f"Error while requesting url '{url}': {e}")
-        return None
+                        if params_header:
+                            first_value, second_value = params_header.split(',')[:2]
+                            # print("first_value:",first_value,"second_value:",second_value)
+                            new_param = '{},100,1'.format(second_value)
+                            if first_value == second_value:
+                                break
+                            headers['param'] = new_param
+                        else:
+                            break
+                    else:
+                        break
+            except Exception as e:
+                print(f"Error while requesting url '{url}': {e}")
+                break
+        return all_data
 
 
-async def process_symbol(session, symbol, url1, url2, results):
+async def process_symbol(symbol, url1, url2, results):
     headers = {
         'token': 'MitakeWeb',
         'symbol': symbol,
         'param': '0,100,-1'
     }
-    evn1_tick, evn2_tick = await asyncio.gather(
-        fetch_data(session, url1, headers),
-        fetch_data(session, url2, headers)
-    )
-    if evn1_tick and evn2_tick:
-        compare_lists(evn1_tick, evn2_tick, symbol, url1, url2, results)
+    evn1_tick = await fetch_data(url1, headers)
+    headers['param'] = '0,100,-1'
+    evn2_tick = await fetch_data(url2, headers)
+    list1_data.append(evn1_tick)
+    list2_data.append(evn2_tick)
+    # print("evn1_tick:",type(evn1_tick),evn1_tick)
+    if list1_data and list2_data:
+        # if symbol == '600000.sh':
+        #     print("list1:",list1)
+        compare_lists(list1_data, list2_data, symbol, url1, url2, results)
 
 
-async def process_file(session, file_path, url1, url2, results):
+async def process_file( file_path, url1, url2, results):
     if not os.path.exists(file_path):
-        print("文件不存在：{}".format(file_path))
+        print(f"文件不存在：{file_path}")
         return
     with open(file_path, 'r', encoding='utf-8') as stock_file:
         stock_list = json.load(stock_file)
         stock_symbols = [stock_item.get('s') for stock_item in stock_list]
-    tasks = [process_symbol(session, symbol, url1, url2, results) for symbol in stock_symbols]
+        # print(len("股票数量为：".format(stock_symbols)))
+    tasks = [process_symbol(symbol, url1, url2, results) for symbol in stock_symbols]
     await asyncio.gather(*tasks)
 
 
 async def main():
     start_time = time.time()
-    async with aiohttp.ClientSession() as session:
-        with pd.ExcelWriter(write_path) as writer:
-            for key, urls in tick_url.items():
-                if key in stock_file_mapping:
-                    file_path = stock_file_mapping[key]
-                    results = []
-                    env1_url = urls.get('url1')
-                    env2_url = urls.get('url2')
-                    if env1_url and env2_url:
-                        await process_file(session, file_path, env1_url, env2_url, results)
-                        if results:
-                            df = pd.DataFrame(results)
-                            df.to_excel(writer, sheet_name=key, index=False)
-                        else:
-                            print("没有{}文件, 跳过写入excel文件".format(key))
+    all_results = {}
+    # async with aiohttp.ClientSession() as session:
+    for key, urls in tick_url.items():
+        if key in stock_file_mapping:
+            file_path = stock_file_mapping[key]
+            results = []
+            env1_url = urls.get('url1')
+            env2_url = urls.get('url2')
+            if env1_url and env2_url:
+                await process_file(file_path, env1_url, env2_url, results)
+                all_results[key] = results
+    if all_results:
+        with pd.ExcelWriter(write_path, engine='xlsxwriter') as writer:
+            for key, result in all_results.items():
+                if result:
+                    df = pd.DataFrame(result)
+                    df.to_excel(writer, sheet_name=key, index=False)
+                else:
+                    empty_df = pd.DataFrame(columns=["代码", "字段", "环境1", "环境2"])
+                    empty_df.to_excel(writer, sheet_name=key, index=False)
 
     end_time = time.time()
     total_time = end_time - start_time
